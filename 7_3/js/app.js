51724163cf7f022175e12c80f3f9e6c353a4b71a
wpiusa <wpiusa@gmail.com>
2017-03-10 09:28:08 -0800

    Add files via upload

---
+++ 7_3/js/app.js
@@ -0,0 +1,15 @@
+var sum=0;
+for (i = 1; sum <=1000; i++) { 
+    sum=sum+i;
+    console.log(sum);
+}
+
+sum=0;
+i=1;
+while (sum <= 1000){
+       
+     sum=sum+i  
+     console.log(sum);          
+     
+     i++;
+}
